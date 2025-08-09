3b735193219f8decac8c8071aa264cfee9c4f93e
restu <restu.akbar.03.04@gmail.com>
2025-08-09 18:09:04 +0700

    fix: tc_pned_001, feat: tc_pned_003

--- tests/TC_PNED.py
+++ tests/TC_PNED.py
@@ -16,7 +16,7 @@ import time
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class TestPengamanan(unittest.TestCase):
+class TC_PNED(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.driver, cls.wait, cls.url = create_driver()
@@ -39,6 +39,10 @@ class TestPengamanan(unittest.TestCase):
 
     #         cls.driver.quit()
 
+    def setUp(self):
+        self.nibar = "167192"
+        self.nama_pemakai = None
+
     def test_TC_PNED_001(self):
         print("TC_PNED_001")
         driver = self.driver
@@ -48,7 +52,7 @@ class TestPengamanan(unittest.TestCase):
             page.is_loaded(page_name="Pengamanan"), "❌ Modul Pengamanan gagal dimuat"
         )
         time.sleep(1)
-        filter_nibar(driver, "167192")
+        filter_nibar(driver, self.nibar)
         time.sleep(1)
         checkbox(driver, identifier=0, by="index", table_selector="table.koptable")
         time.sleep(1)
@@ -61,23 +65,52 @@ class TestPengamanan(unittest.TestCase):
         time.sleep(1)
         filter_nibar(driver, "167192")
         time.sleep(1)
-        cell_value = (
-            WebDriverWait(driver, 10)
-            .until(
-                EC.presence_of_element_located(
-                    (By.XPATH, "//table[@class='koptable']//tbody/tr[1]/td[11]")
-                )
+        row_tds = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, "//table[@class='koptable']//tbody/tr[1]/td")
             )
-            .text
         )
+
+        TC_PNED.nama_pemakai = row_tds[7].text
+        cell_value = row_tds[10].text
+
         assert cell_value == no_ktp_baru, (
             f"[❌] Gagal: nilai no ktp pemakai = {cell_value}, expected = {no_ktp_baru}"
         )
-        print(f"[✅] Edit berhasil — Nomor Identitas Pemakai = {cell_value}")
+        print(
+            f"[✅] TC_PNED_001 berhasil — Nomor Identitas Pemakai yang diperbarui = {cell_value}"
+        )
+        print(
+            "========================================================================"
+        )
 
+    #     def test_TC_PNED_002(self):
+    def test_TC_PNED_003(self):
+        print("TC_PNED_003")
+        driver = self.driver
+        driver.get(f"{self.url}index.php?Pg=05")
+        time.sleep(1)
+        form_input(driver, By.ID, "nodata", self.nibar)
+        time.sleep(1)
+        button(driver, By.XPATH, "//input[@value='Tampilkan']")
+        time.sleep(1)
+        checkbox(driver, identifier=0, by="index", table_selector="table.koptable")
+        time.sleep(1)
+        href_button(driver, "javascript:prosesEdit()")
+        driver.switch_to.window(driver.window_handles[-1])
+        nama_pemakai = (
+            WebDriverWait(driver, 10)
+            .until(EC.presence_of_element_located((By.ID, "nama2")))
+            .get_attribute("value")
+        )
+        assert nama_pemakai == TC_PNED.nama_pemakai, (
+            f"[❌] Gagal: nama pemakai berbeda dengan di pm pemakaian = {nama_pemakai}, expected = {self.nama_pemakai}"
+        )
+        print(f"[✅] TC_PNED_003 berhasil — Nama Identitas Pemakai = {nama_pemakai}")
+        print(
+            "========================================================================"
+        )
 
-#     def test_TC_PNED_002(self):
-#     def test_TC_PNED_003(self):
 
 if __name__ == "__main__":
     unittest.main()
